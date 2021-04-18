13f05b0ba1381dc0cc3a2f65e812dc313ee493ff
Pierre Blanchard <pblanchard@novacel-optical.com>
2021-04-18 13:24:48 +0200

    update(app): Add layout system

---
+++ src/layouts/NavigationLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-main>
+    <v-app-bar
+      clipped-left
+      app
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Optimate</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      app
+    >
+    </v-navigation-drawer>
+    <slot />
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'NavigationLayout',
+  data() {
+    return {
+      drawer: true
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './plugins/router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+
+import SimpleLayout from "./layouts/SimpleLayout"
+import NavigationLayout from "./layouts/NavigationLayout"
 
 Vue.config.productionTip = false
+
+Vue.component("simple-layout", SimpleLayout)
+Vue.component("navigation-layout", NavigationLayout)
 
 new Vue({
   router,
