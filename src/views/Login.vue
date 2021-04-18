fb8a68289a7913c24e89188c31026d10b9fbb8b6
Pierre Blanchard <pblanchard@novacel-optical.com>
2021-04-18 16:05:50 +0200

    update(login): Mock up page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,102 @@
 <template>
-  <div>
-    <h1>This is a login page</h1>
-    <v-btn to="/">Accueil</v-btn>
-  </div>
+  <v-row
+    style="height: 100%"
+    clas="mb-0"
+  >
+    <v-col
+      class="pa-0"
+    >
+      <v-img
+        :src="require('@/assets/login-bg.png')"
+        height="100vh"
+      />
+    </v-col>
+    <v-col class="login-card pa-0">
+      <v-card
+        width="30vw"
+        class="pa-4"
+        style="border-radius: 32px; box-shadow: 5px 5px 60px #A19F9F29;"
+      >
+        <v-toolbar
+          flat
+          dense
+        >
+          <v-toolbar-title class="title-login">Connexion</v-toolbar-title>
+        </v-toolbar>
+        <v-form class="pa-3">
+          <label class="label-input">Identifiant</label>
+          <v-text-field
+            placeholder="Saisissez votre identifiant"
+            solo
+            hide-details
+            dense
+            style="max-width: 350px;"
+            class="mb-2"
+            required
+          >
+            <template v-slot:prepend>
+              <v-chip
+                label
+                class="rounded-lg"
+                color="primary"
+              >
+                <v-icon>mdi-account</v-icon>
+              </v-chip>
+            </template>
+          </v-text-field>
+          <label class="label-input">Mot de passe</label>
+          <v-text-field
+            placeholder="Saisissez votre mot de passe"
+            solo
+            hide-details
+            dense
+            style="max-width: 350px"
+            required
+          >
+            <template v-slot:prepend>
+              <v-chip
+                label
+                class="rounded-lg"
+                color="primary"
+              >
+                <v-icon>mdi-lock</v-icon>
+              </v-chip>
+            </template>
+          </v-text-field>
+        </v-form>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            rounded
+            color="primary"
+            class="pa-4"
+            to="/"
+          >
+            Valider
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
+<style>
+  .login-card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .title-login {
+    color: #0F0F61;
+    letter-spacing: 0px;
+    font-weight: bold;
+    font-size: 30px;
+  }
+  
+  .label-input {
+    color: #979797;
+    letter-spacing: -0.4px;
+    font-size: 20px;
+  }
+</style>
