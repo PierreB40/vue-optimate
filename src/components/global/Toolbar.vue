d7d555087154c94027b3cb80674eddfb982f448d
Pierre Blanchard <pblanchard@novacel-optical.com>
2021-04-20 15:07:03 +0200

    update(layout): Split appbar component

---
+++ src/components/global/Toolbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app-bar
+    color="primary"
+    clipped-left
+    flat
+    app
+  >
+  <v-app-bar-nav-icon
+    @click="$emit('toogle-drawer')"
+    class="ml-3"
+    color="white"
+    x-large
+  />
+  <v-toolbar-title class="pl-0">
+    <v-img
+      :src="require('@/assets/white-logo.png')"
+      width="160"
+    />
+  </v-toolbar-title>
+  <v-divider
+    vertical
+    inset
+    class="ml-4 mr-5"
+    color="white"
+  />
+  <div class="title white--text">{{ title }}</div>
+  <v-spacer />
+  <v-img
+    :src="require('@/assets/logo-sei.png')"
+    style="max-width: 100px"
+    class="mr-4"
+  />
+  <v-btn
+    class="elevation-0 white--text"
+    color="#A60AA8"
+    rounded
+  >
+    M. Dupont
+  </v-btn>
+  <v-btn
+    icon
+    color="white"
+  >
+    <v-icon>mdi-information-outline</v-icon>
+  </v-btn>
+</v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  watch: {
+      $route: {
+          immediate: true,
+          handler(to) {
+              this.title = to.meta.title ? to.meta.title : ""
+          }
+      },
+  }
+}
+</script>
--- src/layouts/NavigationLayout.vue
+++ src/layouts/NavigationLayout.vue
@@ -1,50 +1,8 @@
 <template>
   <v-main>
-    <v-app-bar
-      color="primary"
-      clipped-left
-      flat
-      app
-    >
-      <v-app-bar-nav-icon
-        @click="drawer = !drawer"
-        class="ml-3"
-        color="white"
-        x-large
-      />
-      <v-toolbar-title class="pl-0">
-        <v-img
-          :src="require('@/assets/white-logo.png')"
-          width="160"
-        />
-      </v-toolbar-title>
-      <v-divider
-        vertical
-        inset
-        class="ml-4 mr-5"
-        color="white"
-      />
-      <div class="title white--text">{{ title }}</div>
-      <v-spacer />
-      <v-img
-        :src="require('@/assets/logo-sei.png')"
-        style="max-width: 100px"
-        class="mr-4"
-      />
-      <v-btn
-        class="elevation-0 white--text"
-        color="#A60AA8"
-        rounded
-      >
-        M. Dupont
-      </v-btn>
-      <v-btn
-        icon
-        color="white"
-      >
-        <v-icon>mdi-information-outline</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <toolbar
+      @toogle-drawer="drawer = !drawer"
+    />
     <v-navigation-drawer
       v-model="drawer"
       color="primary"
@@ -74,8 +32,11 @@
 </template>
 
 <script>
+import Toolbar from '@/components/global/Toolbar'
+
 export default {
   name: 'NavigationLayout',
+  components: { Toolbar },
   data() {
     return {
       drawer: false,
@@ -103,14 +64,6 @@ export default {
         }
       ]
     }
-  },
-  watch: {
-      $route: {
-          immediate: true,
-          handler(to) {
-              this.title = to.meta.title ? to.meta.title : ""
-          }
-      },
   }
 }
 </script>
